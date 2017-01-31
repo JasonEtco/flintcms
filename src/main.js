792157e006c8238ff5b97498b01df605a9ad6ccf
jasonetco <jasonetco@gmail.com>
2017-01-31 08:56:32 -0500

    :loop: Setup routing boilerplate

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-console.log('hello!');
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { Router, Route, IndexRoute } from 'react-router';
+import store, { history } from './utils/store';
+
+export default function mapStateToProps(state) {
+  return { ...state };
+}
+
+const App = <div>App!</div>;
+
+const Root = connect(mapStateToProps)(App);
+
+const storeWrapper = (
+  <Provider store={store}>
+    <Router history={history}>
+      <IndexRoute component={Root} />
+      <Route path="test" component={Root} />
+    </Router>
+  </Provider>
+);
+
+render(storeWrapper, document.querySelector('.mount'));
