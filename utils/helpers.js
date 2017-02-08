238d694aaaed12c3e77a604e49a994d033206fc7
JasonEtco <jasonetco@gmail.com>
2017-02-07 20:06:32 -0500

    :wrench: Fix tiny eslint error

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 const helpers = {
   loggedIn(req, res, next) {
     if (req.user !== undefined) return next();
-    res.json({ status: 401, redirect: '/admin/login' });
+    return res.json({ status: 401, redirect: '/admin/login' });
   },
 };
 
