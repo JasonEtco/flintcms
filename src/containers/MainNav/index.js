c3955b137daf36c28610bb072a9de1e41c35c5e1
JasonEtco <jasonetco@gmail.com>
2017-02-11 17:00:26 -0500

    :art: Add icons, style navbar

--- src/containers/MainNav/index.js
+++ src/containers/MainNav/index.js
@@ -1,13 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import FlintLogo from '../../components/FlintLogo';
+import Icon from '../../utils/icons';
 import './MainNav.scss';
 
-const NavItem = props => <li className="nav__list-item"><Link to={props.to}>{props.children}</Link></li>;
+const NavItem = props => <li className="nav__list-item"><Link className="nav__list-item__link" to={props.to}>{props.children}</Link></li>;
 
 NavItem.propTypes = {
   to: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired,
+  children: PropTypes.array.isRequired,
 };
 
 export default class MainNav extends Component {
@@ -16,9 +17,9 @@ export default class MainNav extends Component {
       <nav className="nav">
         <FlintLogo />
         <ul className="nav__list">
-          <NavItem to="/admin">Home</NavItem>
-          <NavItem to="/admin/entries">Entries</NavItem>
-          <NavItem to="/admin/newsection">New Section</NavItem>
+          <NavItem to="/admin"><Icon icon="checkmark" />Home</NavItem>
+          <NavItem to="/admin/entries"><Icon icon="checkmark" />Entries</NavItem>
+          <NavItem to="/admin/newsection"><Icon icon="checkmark" />New Section</NavItem>
         </ul>
       </nav>
     );
