d797abf2f6ad6f5d5b64abc53ddacf0a9eb09769
JasonEtco <jasonetco@gmail.com>
2017-02-14 00:18:33 -0500

    :mag: Add search to table

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 import './Input.scss';
 
 export default class Input extends Component {
@@ -6,27 +7,61 @@ export default class Input extends Component {
     type: PropTypes.oneOf(['text', 'password', 'email']),
     placeholder: PropTypes.string,
     name: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
+    label: PropTypes.string,
     big: PropTypes.bool,
+    full: PropTypes.bool,
+    onChange: PropTypes.func,
+    className: PropTypes.string,
   }
 
   static defaultProps = {
+    label: null,
     type: 'text',
     placeholder: null,
     big: false,
+    full: false,
+    onChange: f => f,
+    className: null,
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    const { name, onChange } = this.props;
+    const { value } = this[name];
+
+    this.value = value;
+    onChange(value);
   }
 
   render() {
-    const { name, label, type, placeholder, big } = this.props;
+    const { name, label, type, placeholder, big, full, className } = this.props;
 
-    if (!label) {
-      return <input className={`input ${big && 'input--big'}`} type={type} name={name} placeholder={placeholder} />;
-    }
+    const classes = classnames(
+      'input',
+      { 'input--big': big },
+      { 'input--full': full },
+      className,
+    );
 
-    return (
+    const input = (
+      <input
+        className={classes}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        onChange={() => this.handleChange()}
+        ref={(r) => { this[name] = r; }}
+      />
+    );
+
+    return !label ? input : (
       <div className="input-wrapper">
         <label className="input__label" htmlFor={name}>{label}</label>
-        <input className={`input ${big && 'input--big'}`} type={type} name={name} id={name} />
+        {input}
       </div>
     );
   }
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import moment from 'moment';
 import h from '../../utils/helpers';
 import p from '../../utils/prettyNames';
+import Input from '../Input';
 import './Table.scss';
 
 const Cell = ({ column, children }) => {
@@ -31,65 +32,99 @@ Cell.defaultProps = { children: '-' };
 export default class Table extends Component {
   static propTypes = {
     data: PropTypes.array.isRequired,
+    showSearch: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    showSearch: true,
   }
 
   constructor(props) {
     super(props);
 
     this.handleSort = this.handleSort.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  state = { sortBy: 'title', direction: 'ASC' }
+  state = { sortBy: 'title', direction: 'ASC', search: '' }
 
   handleSort(sortBy) {
-    console.log(sortBy, this.state.sortBy);
     if (sortBy === this.state.sortBy) {
       this.setState({ direction: this.state.direction === 'ASC' ? 'DESC' : 'ASC' });
     }
     this.setState({ sortBy });
   }
 
+  handleChange(search) {
+    this.setState({ search });
+  }
+
   render() {
-    const { data } = this.props;
-    const columns = data
+    const { data, showSearch } = this.props;
+    const { sortBy, direction, search } = this.state;
+
+    let filtered = data;
+    if (search !== '' && showSearch) {
+      filtered = data.filter((t) => {
+        const re = new RegExp(search.replace(/[/\\^$*+?()|[\]]/g, '\\$&'), 'i');
+        for (const prop in t) {
+          if (typeof t[prop] === 'string' && t[prop].search(re) > -1) return true;
+          if (typeof t[prop] === 'object' && t[prop].value.search(re) > -1) return true;
+        }
+        return false;
+      });
+    }
+
+    const columns = filtered
       .reduce((prev, curr) => [...prev, ...Object.keys(curr)], [])
       .filter((el, i, self) => i === self.indexOf(el));
 
-    const { sortBy, direction } = this.state;
-    const sorted = h.sortArrayOfObjByString(data, sortBy, direction);
+    const sorted = h.sortArrayOfObjByString(filtered, sortBy, direction);
 
     return (
-      <table className="table">
-        <thead>
-          <tr className="table__row">
-            {columns.map((column) => {
-              const btnClass = classnames(
-                'table__header__btn',
-                { 'is-active': sortBy === column },
-                { desc: sortBy === column && direction === 'DESC' },
-                { asc: sortBy === column && direction === 'ASC' },
-              );
+      <div className="table-wrapper">
+        {showSearch &&
+          <Input
+            onChange={this.handleChange}
+            ref={(r) => { this.search = r; }}
+            placeholder="Search"
+            full
+            name="search"
+            className="table__search"
+          />
+        }
+        <table className="table">
+          <thead>
+            <tr className="table__row">
+              {columns.map((column) => {
+                const btnClass = classnames(
+                  'table__header__btn',
+                  { 'is-active': sortBy === column },
+                  { desc: sortBy === column && direction === 'DESC' },
+                  { asc: sortBy === column && direction === 'ASC' },
+                );
 
-              return (
-                <th className="table__header" key={column}>
-                  <button
-                    className={btnClass}
-                    onClick={() => this.handleSort(column)}
-                  >
-                    {p[column]}
-                  </button>
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {sorted.map((tr, i) =>
-            <tr className="table__row" key={i}>{columns.map(column =>
-              <Cell key={tr[column]} column={column}>{tr[column]}</Cell>)}
-            </tr>)}
-        </tbody>
-      </table>
+                return (
+                  <th className="table__header" key={column}>
+                    <button
+                      className={btnClass}
+                      onClick={() => this.handleSort(column)}
+                    >
+                      {p[column]}
+                    </button>
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {sorted.map((tr, i) =>
+              <tr className="table__row" key={i}>{columns.map(column =>
+                <Cell key={tr[column]} column={column}>{tr[column]}</Cell>)}
+              </tr>)}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
