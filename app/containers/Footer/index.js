1d248668971a2e0062d2defb90d9f2ef2f47f11f
JasonEtco <jasonetco@gmail.com>
2017-02-17 17:37:18 -0500

    :eggplant: Style emoji footer

--- app/containers/Footer/index.js
+++ app/containers/Footer/index.js
@@ -1,12 +1,23 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import h from '../../utils/helpers';
 import './Footer.scss';
 
+const baseEmojis = ['🍍', '🕑', '🎏', '🔥', '🦄', '🍑', '🔑', '🙌', '❤️'];
+
 export default class Footer extends Component {
+  state = { emojis: h.shuffle(baseEmojis), i: 0 }
+
+  randomizeEmoji() {
+    const { emojis, i } = this.state;
+    this.setState({ i: i < emojis.length - 1 ? i + 1 : 0 });
+  }
+
   render() {
+    const { emojis, i } = this.state;
+    const btn = <button className="footer__emoji" onClick={() => this.randomizeEmoji()}>{emojis[i]}</button>
     return (
       <footer className="footer">
-        <span>Made with <button className="emoji" onClick={}>❤️</button> on 🌏 by
-          <a href="https://github.com/JasonEtco">Jason</a>
+        <span className="footer__made-with">Made with {btn} on 🌏 by <a href="https://github.com/JasonEtco">Jason Etcovitch</a>
         </span>
       </footer>
     );
--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -20,6 +20,23 @@ const helpers = {
       }
     });
   },
+  shuffle(arr) {
+    const array = arr;
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  },
   isDate(str) {
     return !!Date.parse(str);
   },
