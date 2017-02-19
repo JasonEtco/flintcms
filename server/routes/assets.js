ba43a5c7532e32f759fb6a1344549ba457207b42
JasonEtco <jasonetco@gmail.com>
2017-02-19 11:15:28 -0500

    :wrench: Parse formdata

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const formidable = require('formidable');
+const path = require('path');
+const fs = require('fs');
 const h = require('../utils/helpers');
 
 const Asset = mongoose.model('Asset');
@@ -14,24 +17,50 @@ router.get('/admin/api/assets', h.loggedIn, (req, res) => {
 
 router.post('/admin/api/assets', h.loggedIn, (req, res) => {
   process.nextTick(() => {
-    const newAsset = new Asset();
+    const form = new formidable.IncomingForm();
 
-    newAsset.title = title;
-    newAsset.extension = extension;
-    newAsset.filename = filename;
-    newAsset.width = width;
-    newAsset.height = height;
-    newAsset.size = size;
-    newAsset.mimeType = mimeType;
-    newAsset.dateUploaded = Date.now();
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
 
-    newAsset.save()
-      .then((savedEntry) => {
-        res.status(200).json(savedEntry);
-      })
-      .catch(err => new Error(err));
-  })
-  .catch(err => new Error(err));
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '..', '..', 'assets');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', (field, file) => {
+      fs.rename(file.path, path.join(form.uploadDir, file.name));
+
+      const newAsset = new Asset();
+
+      newAsset.name = file.name;
+      newAsset.extension = 'd';
+      newAsset.filename = file.name;
+      newAsset.width = 3;
+      newAsset.height = 3;
+      newAsset.size = 3;
+      newAsset.mimeType = 'd';
+      newAsset.dateUploaded = Date.now();
+
+      newAsset.save()
+        .then((savedAsset) => {
+          res.status(200).json(savedAsset);
+        })
+        .catch(err => new Error(err));
+    });
+
+    // log any errors that occur
+    form.on('error', (err) => {
+      console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', () => {
+      res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+  });
 });
 
 module.exports = router;
