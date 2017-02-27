fad300829035a95ef27e70d5e8dc4bbe9fb54b4f
JasonEtco <jasonetco@gmail.com>
2017-02-27 13:34:47 -0500

    :wrench: Fix eslint error

--- app/views/Home/index.js
+++ app/views/Home/index.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
     sections: PropTypes.object,
   };
 
-  static defaultTypes = {
+  static defaultProps = {
     entries: null,
     sections: null,
   };
