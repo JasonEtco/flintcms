c3d282942e980dc45922db2cedb4f5c6aceb8232
JasonEtco <jasonetco@gmail.com>
2017-03-06 22:49:11 -0500

    :wrench: Add mock data

---
+++ __mocks__/mockData.js
@@ -0,0 +1,34 @@
+exports.entries = [
+  {
+    _id: '58be1b901810931d043d9fc2',
+    slug: 'test-entry',
+    title: 'Test Entry',
+    section: '58b787fd28044618a8f7a737',
+    author: '589a12b5a08e0c2f24ece4e8',
+    dateCreated: '2017-03-07T02:31:44.010Z',
+    fields: [],
+  },
+  {
+    _id: '58be1b901810931d043d9fc6',
+    slug: 'test-entry-two',
+    title: 'Test Entry Two',
+    section: '58b787fd28044618a8f7a737',
+    author: '589a12b5a08e0c2f24ece4e8',
+    dateCreated: '2017-04-07T02:31:44.010Z',
+    fields: [],
+  },
+];
+
+exports.sections = [
+  {
+    _id: '58b787fd28044618a8f7a737',
+    slug: 'test-section',
+    title: 'Test Section',
+    template: 'index',
+    fields: [
+      '58a232588397e21c8c8c8828',
+      '58a37f8feae5082628cd21e0',
+    ],
+    dateCreated: '2017-03-02T02:48:29.432Z',
+  },
+];
