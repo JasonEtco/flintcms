6158a0c41a1642961e6f931c6ddb61cc574e90f7
jasonetco <jasonetco@gmail.com>
2017-03-21 11:12:08 -0400

    :lock: Destroy session on logout

--- server/apps/routes/auth.js
+++ server/apps/routes/auth.js
@@ -19,6 +19,7 @@ router.post('/login', passport.authenticate('local-login', strategyOptions), (re
 
 router.get('/logout', (req, res) => {
   req.logout();
+  req.session.destroy();
   res.redirect('/');
 });
 
