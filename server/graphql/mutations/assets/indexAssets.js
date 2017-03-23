d5a9b1e2466f94b22b11ccde5a087b797636335d
jasonetco <jasonetco@gmail.com>
2017-03-23 17:39:29 -0400

    [WIP] Use Promise.all for parallel async asset indexing

--- server/graphql/mutations/assets/indexAssets.js
+++ server/graphql/mutations/assets/indexAssets.js
@@ -9,35 +9,35 @@ const { readdirAsync } = require('../../../utils/fsPromises');
 const pathToAssets = path.join(__dirname, '..', '..', '..', '..', 'public', 'assets');
 const Asset = mongoose.model('Asset');
 
-module.exports = {
-  type: new GraphQLObjectType({
-    name: 'AssetIndexer',
-    fields: {
-      savedFiles: { type: new GraphQLList(outputType) },
-      removedFiles: { type: new GraphQLList(outputType) },
-    },
-  }),
-  args: {},
-  async resolve() {
-    // TODO: Use Promise.all to have
-    // removedFiles/savedFiles run at the same time
+/**
+ * Removes file documents from the database
+ * @param {Object[]} dbFiles - Array of Files from the DataBase
+ * @returns {Object[]} An array of file objects that were deleted
+ */
+async function removeFiles(dbFiles) {
+  const removedFiles = await dbFiles
+    .filter(file => !fs.existsSync(path.join(pathToAssets, file.filename)))
+    .map(async (file) => {
+      const deleted = await Asset.findByIdAndRemove(file._id);
+      if (!deleted) throw new Error(`There was a problem deleting an asset: ${file._id}`);
+      return file;
+    });
+  return removedFiles;
+}
 
-    // List of all assets already indexed
-    const dbFiles = await Asset.find().exec();
-
-    // If there is a file in the directory, but does not exist
-    // in the DB, we should add it to the DB.
-
-    // Get current file list
-    const files = await readdirAsync(pathToAssets);
-
-    // Filter out assets already in DB, and assets of wrong format
-    const savedFiles = await files.filter(f =>
+/**
+ * Adds files to the database from the local file system
+ * @param {Object[]} dbFiles - Array of Files from the DataBase
+ * @returns {Object[]} An array of file objects that were saved
+ */
+async function saveFiles(dbFiles) {
+  const files = await readdirAsync(pathToAssets);
+  files.filter(f =>
       // Check if file is in list of indexed assets
       dbFiles.findIndex(dbf => dbf.filename === f) === -1
 
       // TODO: Allow for other file types
-      && f.match(/(?:gif|jpg|png|bmp|jpeg)$/))
+      && f.match(/\.(jpe?g|png|gif|svg)$/i))
     .map(async (file) => {
       const pathToFile = path.resolve(pathToAssets, file);
       const { width, height, mimetype, size } = await getAssetDetails(pathToFile);
@@ -55,17 +55,36 @@ module.exports = {
       if (!savedAsset) throw new Error(`There was a problem saving the asset: ${file}.`);
       return savedAsset;
     });
+}
 
+module.exports = {
+  type: new GraphQLObjectType({
+    name: 'AssetIndexer',
+    fields: {
+      savedFiles: { type: new GraphQLList(outputType) },
+      removedFiles: { type: new GraphQLList(outputType) },
+    },
+  }),
+  args: {},
+  async resolve() {
+    // TODO: Use Promise.all to have
+    // removedFiles/savedFiles run at the same time
 
-    // If there is a document in the DB, but the file doesn't exists
-    // we should remove it from the DB
-    const removedFiles = await dbFiles
-      .filter(file => !fs.existsSync(path.join(pathToAssets, file.filename)))
-      .map(async (file) => {
-        const deleted = await Asset.findByIdAndRemove(file._id);
-        if (!deleted) throw new Error(`There was a problem deleting an asset: ${file._id}`);
-        return file;
-      });
+    // List of all assets already indexed
+    const dbFiles = await Asset.find().exec();
+
+    // const savedFiles = await saveFiles(dbFiles);
+    // const removedFiles = await removeFiles(dbFiles);
+    const vals = await Promise.all([
+      saveFiles(dbFiles),
+      removeFiles(dbFiles),
+    ]);
+
+    console.log(vals);
+
+    // return Promise.all([savedFiles, removedFiles])
+    //   .then(values => ({ savedFiles: values[0], removedFiles: values[1] }))
+    //   .catch(error => new Error(error));
 
     if (!savedFiles || !removedFiles) throw new Error('There was a problem indexing the assets.');
     return { savedFiles, removedFiles };
