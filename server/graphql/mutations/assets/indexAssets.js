610c75bc6d39bff30cad3e57fd6a5939b071a6c4
JasonEtco <jasonetco@gmail.com>
2017-03-23 20:21:48 -0400

    :koala: Use Promise.all for Asset indexing

--- server/graphql/mutations/assets/indexAssets.js
+++ server/graphql/mutations/assets/indexAssets.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose');
 const { outputType } = require('../../types/Assets');
 const { readdirAsync } = require('../../../utils/fsPromises');
 
-const pathToAssets = path.join(__dirname, '..', '..', '..', '..', 'public', 'assets');
 const Asset = mongoose.model('Asset');
 
 /**
@@ -14,7 +13,7 @@ const Asset = mongoose.model('Asset');
  * @param {Object[]} dbFiles - Array of Files from the DataBase
  * @returns {Object[]} An array of file objects that were deleted
  */
-async function removeFiles(dbFiles) {
+async function removeFiles(dbFiles, pathToAssets) {
   const removedFiles = await dbFiles
     .filter(file => !fs.existsSync(path.join(pathToAssets, file.filename)))
     .map(async (file) => {
@@ -30,35 +29,30 @@ async function removeFiles(dbFiles) {
  * @param {Object[]} dbFiles - Array of Files from the DataBase
  * @returns {Object[]} An array of file objects that were saved
  */
-async function saveFiles(dbFiles) {
+async function saveFiles(dbFiles, pathToAssets) {
   const files = await readdirAsync(pathToAssets);
-  const filtered = await files.filter(f =>
-    // Check if file is in list of indexed assets
-    dbFiles.findIndex(dbf => dbf.filename === f) === -1
+  const savedFiles = await files
+    .filter(f =>
+      dbFiles.findIndex(dbf => dbf.filename === f) === -1 &&
+      f.match(/(?:gif|jpg|png|bmp|jpeg)$/))
+    .map(async (file) => {
+      const pathToFile = path.resolve(pathToAssets, file);
+      const { width, height, mimetype, size } = await getAssetDetails(pathToFile);
 
-    // TODO: Allow for other file types
-    && f.match(/\.(jpe?g|png|gif|svg)$/i));
+      const newAsset = new Asset({
+        title: file,
+        filename: file,
+        size,
+        width,
+        height,
+        mimetype,
+      });
 
-  const ret = await filtered.map(async (file) => {
-    const pathToFile = path.resolve(pathToAssets, file);
-    const { width, height, mimetype, size } = await getAssetDetails(pathToFile);
-
-    const newAsset = new Asset({
-      title: file,
-      filename: file,
-      size,
-      width,
-      height,
-      mimetype,
+      const savedAsset = await newAsset.save();
+      if (!savedAsset) throw new Error(`There was a problem saving the asset: ${file}.`);
+      return savedAsset;
     });
-
-    const savedAsset = await newAsset.save();
-    if (!savedAsset) throw new Error(`There was a problem saving the asset: ${file}.`);
-    return savedAsset;
-  });
-
-  console.log(ret);
-  return ret;
+  return savedFiles;
 }
 
 module.exports = {
@@ -71,27 +65,13 @@ module.exports = {
   }),
   args: {},
   async resolve() {
-    // TODO: Use Promise.all to have
-    // removedFiles/savedFiles run at the same time
-
-    // List of all assets already indexed
+    const pathToAssets = path.join(__dirname, '..', '..', '..', '..', 'public', 'assets');
     const dbFiles = await Asset.find().exec();
 
-    const savedFiles = await saveFiles(dbFiles);
-    const removedFiles = await removeFiles(dbFiles);
-    // const vals = await Promise.all([
-    //   saveFiles(dbFiles),
-    //   removeFiles(dbFiles),
-    // ]);
-
-    console.log(savedFiles, removedFiles);
-
-    // return Promise.all([savedFiles, removedFiles])
-    //   .then(values => ({ savedFiles: values[0], removedFiles: values[1] }))
-    //   .catch(error => new Error(error));
-
-    // if (!savedFiles || !removedFiles) throw new Error('There was a problem indexing the assets.');
-    // return { savedFiles, removedFiles };
-    return { savedFiles: [], removedFiles: [] };
+    const [savedFiles, removedFiles] = await Promise.all([
+      saveFiles(dbFiles, pathToAssets),
+      removeFiles(dbFiles, pathToAssets),
+    ]);
+    return { savedFiles, removedFiles };
   },
 };
