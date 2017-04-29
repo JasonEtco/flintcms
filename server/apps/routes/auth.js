e36b531ebf87ff2d34408f325cf4bdf7a2db9e52
JasonEtco <jasonetco@gmail.com>
2017-04-28 20:47:45 -0400

    :lock: Fix logout bug

--- server/apps/routes/auth.js
+++ server/apps/routes/auth.js
@@ -29,9 +29,7 @@ router.get('/verify', async (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout();
-  req.session.destroy(() => {
-    res.redirect('/');
-  });
+  res.redirect('/');
 });
 
 
