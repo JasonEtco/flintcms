4b8e992fd4717f05cf5d379091fc815b2f86efc2
JasonEtco <jasonetco@gmail.com>
2017-06-06 00:54:21 -0400

    :wrench: Switch yarn for package-lock, remove unused packages/scripts

--- server/utils/helpers.test.js
+++
@@ -1,7 +0,0 @@
-const helpers = require('./helpers');
-
-describe('slugify', () => {
-  it('should convert a string to kebab-case', () => {
-    expect(helpers.slugify('This is a test!')).toBe('this-is-a-test');
-  });
-});
