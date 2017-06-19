a20c315e6498da19b613c63e9e6b7efa841a630f
Jason Etcovitch <jasonetco@github.com>
2017-06-18 19:55:12 -0700

    :art: Watch SCSS files for changes, then recompile. Fixes #21

--- server/utils/compileSass.js
+++ server/utils/compileSass.js
@@ -1,6 +1,8 @@
 const sass = require('node-sass');
 const path = require('path');
+const fs = require('fs');
 const chalk = require('chalk');
+const chokidar = require('chokidar');
 const scaffold = require('./scaffold');
 const { writeFileAsync } = require('./fsPromises');
 
@@ -14,28 +16,69 @@ function sassAsync(opt) {
 }
 
 /**
+ * Actually compiles the SCSS
+ * @param {object} opts - Compile options
+ */
+async function compile() {
+  const opts = {
+    file: path.join(global.FLINT.scssPath, global.FLINT.scssEntryPoint),
+    outputStyle: process.env.NODE_ENV === 'production' ? 'compressed' : 'nested',
+  };
+
+  try {
+    const scss = await sassAsync(opts);
+
+    const cssPath = path.join(global.FLINT.publicPath);
+    const p = await scaffold(cssPath);
+    const filename = global.FLINT.scssEntryPoint.replace('.scss', '.css');
+    const pathToFile = path.join(p, filename);
+    if (!fs.existsSync(pathToFile)) return `${chalk.red('[SCSS]')} The scssEntryPoint does not exist.`;
+
+    await writeFileAsync(pathToFile, scss.css);
+    return `${chalk.grey('[SCSS]')} Your SCSS has been compiled to ${pathToFile}`;
+  } catch (e) {
+    /* eslint-disable no-console */
+    console.log(`  ${chalk.grey('Message:')} ${chalk.red(e.message)}`);
+    console.log(`  ${chalk.grey('Line:')} ${chalk.red(e.line)}`);
+    console.log(`  ${chalk.grey('File:')} ${chalk.red(e.file)}`);
+    /* eslint-enable no-console */
+
+    return `${chalk.red('[SCSS]')} There was an error compiling your SCSS.`;
+  }
+}
+
+function recompile() {
+  // eslint-disable-next-line no-console
+  console.log(chalk.cyan('Recompiling SASS...'));
+  return compile();
+}
+
+function watch(watcher) {
+  watcher.on('add', async () => {
+    const compiled = await recompile();
+    console.log(compiled); // eslint-disable-line no-console
+  });
+  watcher.on('change', async () => {
+    const compiled = await recompile();
+    console.log(compiled); // eslint-disable-line no-console
+  });
+}
+
+/**
  * Compiles SASS using the scssEntryPoint config
  */
 async function compileSass() {
   if (global.FLINT.scssEntryPoint) {
-    const opts = {
-      file: path.join(global.FLINT.scssPath, global.FLINT.scssEntryPoint),
-      outputStyle: process.env.NODE_ENV === 'production' ? 'compressed' : 'nested',
-    };
+    if (global.FLINT.debugMode) {
+      const watcher = chokidar.watch(global.FLINT.scssPath, {
+        persistent: true,
+        ignoreInitial: true,
+      });
 
-    try {
-      const scss = await sassAsync(opts);
-
-      const cssPath = path.join(global.FLINT.publicPath);
-      const p = await scaffold(cssPath);
-      const filename = global.FLINT.scssEntryPoint.replace('.scss', '.css');
-      const pathToFile = path.join(p, filename);
-
-      await writeFileAsync(pathToFile, scss.css);
-      return `${chalk.grey('[SCSS]')} Your SCSS has been compiled to ${pathToFile}`;
-    } catch (e) {
-      return `${chalk.red('[SCSS]')} There was an error compiling your SCSS. Check your site configuration.`;
+      watch(watcher);
     }
+
+    return compile();
   }
 
   return chalk.grey('SCSS compiling has been disabled in the site configuration.');
