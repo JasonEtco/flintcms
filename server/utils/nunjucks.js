380d2e68561622c0ad8099398295e1fde6afffe8
Jason Etcovitch <jasonetco@github.com>
2017-06-18 21:26:04 -0700

    :date: Add date filter

--- server/utils/nunjucks.js
+++ server/utils/nunjucks.js
@@ -1,4 +1,5 @@
 const nunjucks = require('nunjucks');
+const dateFilter = require('nunjucks-date-filter');
 
 const nun = nunjucks.configure(global.FLINT.templatePath, {
   noCache: process.env.NODE_ENV !== 'production',
@@ -11,6 +12,7 @@ Object.keys(global.FLINT).forEach((key) => {
 nun.addGlobal('getContext', () => this.ctx);
 
 nun.addFilter('json', obj => `<pre><code>${JSON.stringify(obj, null, 2)}</code></pre>`);
+nun.addFilter('date', dateFilter);
 
 nunjucks.precompile(global.FLINT.templatePath, { env: nun });
 
