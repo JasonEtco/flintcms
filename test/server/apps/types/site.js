7b6f2088ebc87da7309dbd3a577d9b257297d438
Jason Etcovitch <jasonetco@github.com>
2017-08-28 00:03:44 -0700

    Add tests for site config

--- test/server/apps/types/site.js
+++ test/server/apps/types/site.js
@@ -1,5 +1,6 @@
 const mocks = require('../../../mocks');
 const expect = require('expect');
+const common = require('../common');
 
 it('returns the site config', (done) => {
   global.agent
@@ -27,4 +28,64 @@ it('returns the site config', (done) => {
       });
       return done();
     });
+});
+
+it('updates the site document', (done) => {
+  global.agent
+    .post('/graphql')
+    .send({
+      query: `mutation ($data: SiteInput!) {
+        updateSite (data: $data) {
+          siteName
+        }
+      }`,
+      variables: {
+        data: {
+          siteName: 'New site name',
+        },
+      },
+    })
+    .end((err, res) => {
+      if (err) { return done(err); }
+      expect(JSON.parse(res.text)).toEqual({
+        data: {
+          updateSite: {
+            siteName: 'New site name',
+          },
+        },
+      });
+      return done();
+    });
+});
+
+describe('Permissions', function () {
+  before('Set to non-admin', common.setNonAdmin);
+
+  it('cannot update the site document', (done) => {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `mutation ($data: SiteInput!) {
+          updateSite (data: $data) {
+            siteName
+            siteUrl
+            style
+          }
+        }`,
+        variables: {
+          data: {
+            siteName: 'New site name',
+          },
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to manage site configuration.',
+        });
+        return done();
+      });
+  });
+
+  after('Set to admin', common.setAdmin);
 });
