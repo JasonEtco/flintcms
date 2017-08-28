60e03acb2630b2c9a920cfb1402789203cb3d557
Jason Etcovitch <jasonetco@github.com>
2017-08-27 20:44:58 -0700

    Add some more entry tests

--- test/server/apps/types/entries.js
+++ test/server/apps/types/entries.js
@@ -1,5 +1,6 @@
 const mocks = require('../../../mocks');
 const expect = require('expect');
+const common = require('../common');
 
 it('returns a list of entries', (done) => {
   global.agent
@@ -107,4 +108,110 @@ it('can save an entry to the database', function (done) {
       });
       return done();
     });
+});
+
+it('can update an entry in the database', function (done) {
+  global.agent
+    .post('/graphql')
+    .send({
+      query: `
+      mutation ($_id: ID!, $data: EntriesInput!) {
+        updateEntry (_id: $_id, data: $data) {
+          title
+        }
+      }`,
+      variables: {
+        _id: mocks.entries[0]._id,
+        data: {
+          title: 'New title!',
+          author: mocks.users[0]._id,
+          section: mocks.sections[0]._id,
+          fields: [{
+            fieldId: mocks.fields[0]._id,
+            handle: mocks.fields[0].handle,
+            value: 'Hello!',
+          }],
+        },
+      },
+    })
+    .end((err, res) => {
+      if (err) { return done(err); }
+      expect(JSON.parse(res.text)).toEqual({
+        data: {
+          updateEntry: {
+            title: 'New title!',
+          },
+        },
+      });
+      return done();
+    });
+});
+
+describe('Permissions', function () {
+  before('Set to non-admin', common.setNonAdmin);
+
+  it('throws when user is not allowed to add a new entry', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `mutation ($data: EntriesInput!) {
+          addEntry (data: $data) {
+            title
+          }
+        }`,
+        variables: {
+          data: {
+            title: 'This should not work',
+            author: mocks.users[0]._id,
+            section: mocks.sections[0]._id,
+            fields: [{
+              fieldId: mocks.fields[0]._id,
+              handle: mocks.fields[0].handle,
+              value: 'Hello!',
+            }],
+          },
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to create new Entries',
+        });
+        return done();
+      });
+  });
+
+  it('throws when user is not allowed to update an entry', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `mutation ($_id: ID!, $data: EntriesInput!) {
+          updateEntry (_id: $_id, data: $data) {
+            title
+          }
+        }`,
+        variables: {
+          _id: mocks.entries[0]._id,
+          data: {
+            title: 'New title!',
+            author: mocks.users[0]._id,
+            section: mocks.sections[0]._id,
+            fields: [{
+              fieldId: mocks.fields[0]._id,
+              handle: mocks.fields[0].handle,
+              value: 'Hello!',
+            }],
+          },
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You are not allowed to change the status of entries. Sorry!',
+        });
+        return done();
+      });
+  });
+
+  after('Set to admin', common.setAdmin);
 });
