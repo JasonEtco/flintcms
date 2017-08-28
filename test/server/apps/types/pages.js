e8ad692ec8a549a179d74ed15ee7bdf654561188
Jason Etcovitch <jasonetco@github.com>
2017-08-28 16:27:38 -0700

    Add more pages tests

--- test/server/apps/types/pages.js
+++ test/server/apps/types/pages.js
@@ -1,5 +1,6 @@
 const mocks = require('../../../mocks');
 const expect = require('expect');
+const common = require('../common');
 
 it('returns a list of pages', (done) => {
   global.agent
@@ -111,4 +112,147 @@ it('can save a page to the database', function (done) {
       });
       return done();
     });
+});
+
+it('sets a new homepage\'s route to `/`', function (done) {
+  global.agent
+    .post('/graphql')
+    .send({
+      query: `
+      mutation ($data: PagesInput!) {
+        addPage (data: $data) {
+          route
+        }
+      }`,
+      variables: {
+        data: {
+          title: 'New Homepage',
+          homepage: true,
+          route: '/pizza',
+          template: mocks.pages[0].template,
+          fieldLayout: [mocks.fields[0]._id],
+        },
+      },
+    })
+    .end((err, res) => {
+      if (err) { return done(err); }
+      expect(JSON.parse(res.text)).toEqual({
+        data: {
+          addPage: {
+            route: '/',
+          },
+        },
+      });
+      return done();
+    });
+});
+
+it('overwrites the last homepage when a new homepage is saved', function (done) {
+  global.agent
+    .post('/graphql')
+    .send({
+      query: `{
+        pages {
+          homepage
+        }
+      }`,
+    })
+    .end((err, res) => {
+      if (err) { return done(err); }
+      expect(JSON.parse(res.text)).toEqual({
+        data: {
+          pages: [
+            { homepage: false },
+            { homepage: true },
+          ],
+        },
+      });
+      return done();
+    });
+});
+
+describe('Permissions', function () {
+  before('Set to non-admin', common.setNonAdmin);
+
+  it('returns an error when adding a page', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `
+        mutation ($data: PagesInput!) {
+          addPage (data: $data) {
+            _id
+          }
+        }`,
+        variables: {
+          data: {
+            title: 'New page',
+            route: '/pizza',
+            template: mocks.pages[0].template,
+            fieldLayout: [mocks.fields[0]._id],
+          },
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to create a new Page.',
+        });
+        return done();
+      });
+  });
+
+  it('returns an error when editing a page', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `
+        mutation ($_id: ID!, $data: PagesInput!) {
+          updatePage (_id: $_id, data: $data) {
+            _id
+          }
+        }`,
+        variables: {
+          _id: mocks.pages[0]._id,
+          data: {
+            title: 'New page',
+            route: '/pizza',
+            template: mocks.pages[0].template,
+            fieldLayout: [mocks.fields[0]._id],
+          },
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to edit Pages.',
+        });
+        return done();
+      });
+  });
+
+  it('returns an error when deleting a page', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `
+        mutation ($_id: ID!) {
+          removePage (_id: $_id) {
+            _id
+          }
+        }`,
+        variables: {
+          _id: mocks.pages[0]._id,
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to delete Pages.',
+        });
+        return done();
+      });
+  });
+
+  after('Set to admin', common.setAdmin);
 });
