9f935b3f59215d418a191771811afa4cc7eeab38
Jason Etcovitch <jasonetco@github.com>
2017-08-28 18:30:22 -0700

    Add more usergroup tests

--- test/server/apps/types/usergroups.js
+++ test/server/apps/types/usergroups.js
@@ -1,6 +1,7 @@
 const mocks = require('../../../mocks');
 const expect = require('expect');
 const permissions = require('../../../../server/utils/permissions.json');
+const common = require('../common');
 
 const permissionsQuery = `
   permissions {
@@ -52,6 +53,37 @@ it('can query for a specific usergroup', function (done) {
       expect(JSON.parse(res.text)).toEqual({
         data: {
           usergroup: { _id: mocks.usergroups[0]._id },
+        },
+      });
+      return done();
+    });
+});
+
+it('can update a usergroup in the database', function (done) {
+  global.agent
+    .post('/graphql')
+    .send({
+      query: `
+      mutation ($_id: ID!, $data: UserGroupInput!) {
+        updateUserGroup (_id: $_id, data: $data) {
+          title
+        }
+      }`,
+      variables: {
+        _id: mocks.usergroups[1]._id,
+        data: {
+          title: 'New title!',
+          permissions: mocks.usergroups[1].permissions,
+        },
+      },
+    })
+    .end((err, res) => {
+      if (err) { return done(err); }
+      expect(JSON.parse(res.text)).toEqual({
+        data: {
+          updateUserGroup: {
+            title: 'New title!',
+          },
         },
       });
       return done();
@@ -111,4 +143,58 @@ it('can save a usergroup to the database', function (done) {
       });
       return done();
     });
+});
+
+describe('Permissions', function () {
+  before('Set to non-admin', common.setNonAdmin);
+
+  it('cannot delete a usergroup from the database', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `
+        mutation ($_id: ID!) {
+          removeUserGroup (_id: $_id) {
+            _id
+          }
+        }`,
+        variables: { _id: mocks.usergroups[1]._id },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to delete User Groups.',
+        });
+        return done();
+      });
+  });
+
+  it('cannot save a usergroup to the database', function (done) {
+    global.agent
+      .post('/graphql')
+      .send({
+        query: `
+        mutation ($data: UserGroupInput!) {
+          addUserGroup (data: $data) {
+            title
+            ${permissionsQuery}
+          }
+        }`,
+        variables: {
+          data: {
+            title: 'Pizza Group',
+            permissions: mocks.usergroups[1].permissions,
+          },
+        },
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        expect(JSON.parse(res.text).errors[0]).toInclude({
+          message: 'You do not have permission to add User Groups.',
+        });
+        return done();
+      });
+  });
+
+  after('Set to admin', common.setAdmin);
 });
