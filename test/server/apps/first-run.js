2db844a8cbe7ee2265db6d970706636c43ebcdfc
Jason Etcovitch <jasonetco@gmail.com>
2017-09-06 19:35:25 -0400

    More tests for first user

--- test/server/apps/first-run.js
+++ test/server/apps/first-run.js
@@ -2,6 +2,7 @@ const supertest = require('supertest');
 const mocks = require('../../mocks');
 const Flint = require('../../../index.js');
 const mongoose = require('mongoose');
+const expect = require('chai').expect;
 
 describe('First time install', function () {
   let server;
@@ -15,12 +16,19 @@ describe('First time install', function () {
     return server;
   });
 
-  it('creates a first new user', function (done) {
-    agent
-      .post('/admin/firstuser')
-      .send(mocks.user)
-      .expect(200)
-      .end(done);
+  it('creates a first new user', async function () {
+    const res = await agent.post('/admin/firstuser').send(mocks.user);
+    expect(res.status).to.equal(200);
+    expect(JSON.parse(res.text)).to.deep.equal({ success: true });
+  });
+
+  it('returns a message when a user already exists', async function () {
+    const res = await agent.post('/admin/firstuser').send(mocks.user);
+    expect(res.status).to.equal(200);
+    expect(JSON.parse(res.text)).to.deep.equal({
+      success: false,
+      message: 'There is already a user in the database.',
+    });
   });
 
   it('logs in a user', function (done) {
