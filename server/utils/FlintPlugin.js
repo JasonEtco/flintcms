153de6466adb2033640a45ea6c1602f18ec92812
JasonEtco <jasonetco@gmail.com>
2017-09-09 15:42:07 -0400

    Fix plugin test logs

--- server/utils/FlintPlugin.js
+++ server/utils/FlintPlugin.js
@@ -1,6 +1,7 @@
 const events = require('./events');
 const path = require('path');
 const chalk = require('chalk');
+const log = require('debug')('flint:plugin');
 
 /**
  * Flint Plugin Class
@@ -13,7 +14,7 @@ class FlintPlugin {
 
   static get uid() {
     // eslint-disable-next-line no-console
-    console.error(chalk.red('A plugin forgot to set a static getter for the uid. See https://flintcms.co/docs/plugins for more information.'));
+    log(chalk.red('A plugin forgot to set a static getter for the uid. See https://flintcms.co/docs/plugins for more information.'));
   }
 
   /**
@@ -32,9 +33,8 @@ class FlintPlugin {
   static get model() { return {}; }
 
   init() {
-    /* eslint-disable no-console */
-    console.log(`Welcome to the ${this.name} plugin! You have forgotten to create your init class method. Oh well :(`);
-    console.log('The init method of your plugin is the entry point for Flint to know how to deal with the plugin, set up hooks and generally deal with the plugin.');
+    log(`Welcome to the ${this.name} plugin! You have forgotten to create your init class method. Oh well :(`);
+    log('The init method of your plugin is the entry point for Flint to know how to deal with the plugin, set up hooks and generally deal with the plugin.');
   }
 }
 
