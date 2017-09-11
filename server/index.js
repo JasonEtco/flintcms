87e8777cf6191277b34f41c27a3ff6ec736a17b1
JasonEtco <jasonetco@gmail.com>
2017-09-10 22:20:33 -0400

    Disable morgan in test

--- server/index.js
+++ server/index.js
@@ -20,8 +20,11 @@ module.exports = () => {
   const server = shutter(Server(app));
   app.set('io', io(server));
 
-  const accessLogStream = fs.createWriteStream(path.join(global.FLINT.logsPath, 'http-requests.log'), { flags: 'a' });
-  app.use(morgan('combined', { stream: accessLogStream }));
+  /* istanbul ignore if */
+  if (process.env.NODE_ENV !== 'test') {
+    const accessLogStream = fs.createWriteStream(path.join(global.FLINT.logsPath, 'http-requests.log'), { flags: 'a' });
+    app.use(morgan('combined', { stream: accessLogStream }));
+  }
 
   app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: true }));
