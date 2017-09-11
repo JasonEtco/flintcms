601bd88e2b57caa2167865257a40bd21dab69bb4
JasonEtco <jasonetco@gmail.com>
2017-09-11 13:58:31 -0400

    Delete unused code

--- server/graphql/types/Assets.js
+++ server/graphql/types/Assets.js
@@ -5,28 +5,8 @@ const {
   GraphQLID,
   GraphQLInt,
   GraphQLInputObjectType,
-  GraphQLBuffer,
 } = require('graphql');
 const { DateTime } = require('./CustomTypes');
-
-exports.file = new GraphQLInputObjectType({
-  name: 'File',
-  description: 'A file uploaded via multipart/form-data',
-  fields: () => ({
-    fieldname: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The fieldname used to POST this file.',
-    },
-    originalname: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The original file name.',
-    },
-    buffer: {
-      type: new GraphQLNonNull(GraphQLBuffer),
-      description: 'The file buffered in memory',
-    },
-  }),
-});
 
 exports.outputType = new GraphQLObjectType({
   name: 'Asset',
