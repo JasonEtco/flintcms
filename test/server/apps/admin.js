1b75705a014472e911dedf9ee0340dca697968f6
JasonEtco <jasonetco@gmail.com>
2017-09-11 15:04:31 -0400

    Tweak admin route tests

--- test/server/apps/admin.js
+++ test/server/apps/admin.js
@@ -1,9 +1,9 @@
 const Flint = require('../../../index.js');
+const expect = require('chai').expect;
 const request = require('supertest');
 const mongoose = require('mongoose');
 
-describe('admin', function () {
-  this.timeout(4000);
+describe('admin routes', function () {
   let server;
 
   before('Creates a server', async function () {
@@ -12,20 +12,16 @@ describe('admin', function () {
     return server;
   });
 
-  describe('GET /admin/*', function () {
-    it('returns a 200 response for /admin/login', function (done) {
-      request(server).get('/admin/login')
-        .expect(200)
-        .expect(res => res.text.startsWith('<!DOCTYPE html>'))
-        .end(done);
-    });
+  it('returns the correct response for /admin/login', async function () {
+    const res = await request(server).get('/admin/login');
+    expect(res.status).to.equal(200);
+    expect(res.text).to.include('<!DOCTYPE html>');
+  });
 
-    it('returns a 200 response for any route', function (done) {
-      request(server).get('/admin/asdfsdfsadfsadf')
-        .expect(200)
-        .expect(res => res.text.startsWith('<!DOCTYPE html>'))
-        .end(done);
-    });
+  it('returns the correct response for any route', async function () {
+    const res = await request(server).get('/admin/asdfsdfsadfsadf');
+    expect(res.status).to.equal(200);
+    expect(res.text).to.include('<!DOCTYPE html>');
   });
 
   after('Closes the server', function (done) {
