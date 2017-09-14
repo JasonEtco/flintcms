260b22d2fd80641fefad91e6111e7e5082659a21
JasonEtco <jasonetco@gmail.com>
2017-09-14 15:39:34 -0400

    Use react-rte

--- app/components/Fields/RichText/index.js
+++ app/components/Fields/RichText/index.js
@@ -3,47 +3,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { stateToHTML } from 'draft-js-export-html';
-import {
-  CompositeDecorator,
-  Editor,
-  EditorState,
-  RichUtils,
-  ContentState,
-  convertFromHTML,
-} from 'draft-js';
-import Icon from 'utils/icons';
+import RichTextEditor from 'react-rte';
 import { formatStringWithCode } from 'utils/helpers';
-import ToolBar from './ToolBar';
 import './RichText.scss';
-
-function findLinkEntities(contentBlock, callback, contentState) {
-  contentBlock.findEntityRanges(
-    (character) => {
-      const entityKey = character.getEntity();
-      return (
-        entityKey !== null &&
-        contentState.getEntity(entityKey).getType() === 'LINK'
-      );
-    },
-    callback,
-  );
-}
-
-const Link = (props) => {
-  const { url } = props.contentState.getEntity(props.entityKey).getData();
-  return (
-    <a href={url} style={{ color: '#3b5998', textDecoration: 'underline' }}>
-      {props.children}
-    </a>
-  );
-};
-
-Link.propTypes = {
-  contentState: PropTypes.object.isRequired,
-  children: PropTypes.array.isRequired,
-  entityKey: PropTypes.string.isRequired,
-};
 
 export default class RichText extends Component {
   static propTypes = {
@@ -67,161 +29,21 @@ export default class RichText extends Component {
 
   constructor(props) {
     super(props);
-    this.toggleInlineStyle = this.toggleInlineStyle.bind(this);
-    this.toggleBlockType = this.toggleBlockType.bind(this);
-
-    const decorator = new CompositeDecorator([
-      {
-        strategy: findLinkEntities,
-        component: Link,
-      },
-    ]);
-
     if (props.defaultValue) {
-      const blocksFromHTML = convertFromHTML(props.defaultValue);
-      const state = ContentState.createFromBlockArray(
-        blocksFromHTML.contentBlocks,
-        blocksFromHTML.entityMap,
-      );
-
-      this.state = {
-        editorState: EditorState.createWithContent(state, decorator),
-        showURLInput: false,
-        value: props.defaultValue,
-        urlValue: '',
-      };
+      const value = RichTextEditor.createValueFromString(props.defaultValue, 'html');
+      this.state = { value };
     } else {
-      try {
-        const editorState = EditorState.createEmpty(decorator);
-        this.state = {
-          editorState,
-          showURLInput: false,
-          value: '',
-          urlValue: '',
-        };
-      } catch (e) {
-        throw new Error('Editor State Error!');
-      }
+      const value = RichTextEditor.createEmptyValue();
+      this.state = { value };
     }
 
     this.focus = () => this[props.name].focus();
-    this.onChange = editorState => this.setState({
-      editorState,
-      value: stateToHTML(editorState.getCurrentContent()),
-    });
-
-    this.promptForLink = this.promptForLink.bind(this);
-    this.onURLChange = e => this.setState({ urlValue: e.target.value });
-    this.confirmLink = this.confirmLink.bind(this);
-    this.onLinkInputKeyDown = this.onLinkInputKeyDown.bind(this);
-    this.removeLink = this.removeLink.bind(this);
-  }
-
-  onLinkInputKeyDown(e) {
-    if (e.which === 13) {
-      this.confirmLink(e);
-    } else if (e.which === 27) {
-      this.setState({ showURLInput: false });
-    }
-  }
-
-  promptForLink(e) {
-    e.preventDefault();
-    const { editorState } = this.state;
-    const selection = editorState.getSelection();
-    if (!selection.isCollapsed()) {
-      const contentState = editorState.getCurrentContent();
-      const startKey = editorState.getSelection().getStartKey();
-      const startOffset = editorState.getSelection().getStartOffset();
-      const blockWithLinkAtBeginning = contentState.getBlockForKey(startKey);
-      const linkKey = blockWithLinkAtBeginning.getEntityAt(startOffset);
-
-      let url = '';
-      if (linkKey) {
-        const linkInstance = contentState.getEntity(linkKey);
-        url = linkInstance.getData().url;
-      }
-
-      this.setState({
-        showURLInput: true,
-        urlValue: url,
-      }, () => {
-        setTimeout(() => this.url.focus(), 0);
-      });
-    }
-  }
-
-  confirmLink(e) {
-    e.preventDefault();
-    const { editorState, urlValue } = this.state;
-    const contentState = editorState.getCurrentContent();
-    const contentStateWithEntity = contentState.createEntity(
-      'LINK',
-      'MUTABLE',
-      { url: urlValue },
-    );
-    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
-    const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
-    this.setState({
-      editorState: RichUtils.toggleLink(
-        newEditorState,
-        newEditorState.getSelection(),
-        entityKey,
-      ),
-      showURLInput: false,
-      urlValue: '',
-    }, () => {
-      setTimeout(() => this[this.props.name].focus(), 0);
-    });
-  }
-
-  removeLink(e) {
-    e.preventDefault();
-    const { editorState } = this.state;
-    const selection = editorState.getSelection();
-    if (!selection.isCollapsed()) {
-      this.setState({
-        editorState: RichUtils.toggleLink(editorState, selection, null),
-      });
-    }
-  }
-
-  toggleInlineStyle(style) {
-    const { editorState } = this.state;
-    this.onChange(RichUtils.toggleInlineStyle(editorState, style));
-  }
-
-  toggleBlockType(blockType) {
-    const { editorState } = this.state;
-    this.onChange(RichUtils.toggleBlockType(editorState, blockType));
+    this.onChange = value => this.setState({ value });
   }
 
   render() {
-    const { editorState, showURLInput, urlValue } = this.state;
+    const { value } = this.state;
     const { name, label, instructions, required } = this.props;
-
-    let urlInput;
-    if (showURLInput) {
-      urlInput = (
-        <div className="rich-text__url">
-          <input
-            onChange={this.onURLChange}
-            ref={(r) => { this.url = r; }}
-            className="rich-text__url__input input"
-            type="text"
-            placeholder="http://www.example.com"
-            value={urlValue}
-            onKeyDown={this.onLinkInputKeyDown}
-          />
-          <button className="rich-text__url__btn" type="button" onClick={this.confirmLink}>
-            Confirm
-          </button>
-          <button className="rich-text__url__close" type="button" onClick={() => this.setState({ showURLInput: false })}>
-            <Icon icon="cross" width={10} height={10} />
-          </button>
-        </div>
-      );
-    }
 
     const classes = classnames(
       'rich-text-wrapper',
@@ -235,23 +57,13 @@ export default class RichText extends Component {
         {instructions
           && <p className="input__instructions" dangerouslySetInnerHTML={{ __html: formatStringWithCode(instructions) }} /> // eslint-disable-line react/no-danger
         }
-        <ToolBar
-          editorState={editorState}
-          toggleInlineStyle={this.toggleInlineStyle}
-          toggleBlockType={this.toggleBlockType}
-          removeLink={this.removeLink}
-          promptForLink={this.promptForLink}
-          urlInput={urlInput}
+        <RichTextEditor
+          value={value}
+          onChange={this.onChange}
+          toolbarClassName="rich-text-toolbar"
+          editorClassName="rich-text-editor"
         />
-        <div className="rich-text__editor" onClick={this.focus}>
-          <Editor
-            editorState={editorState}
-            onChange={this.onChange}
-            placeholder="Enter some text..."
-            ref={(r) => { this[name] = r; }}
-          />
-        </div>
-        <input type="text" readOnly required={required} hidden name={name} value={this.state.value} />
+        <input type="text" readOnly required={required} hidden name={name} value={value.toString('html')} />
       </div>
     );
   }
