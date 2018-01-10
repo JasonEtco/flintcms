364ecdddf9621539ab794df48311652e798328a8
Derek Kuschel <dkuschel@hugeinc.com>
2018-01-10 01:02:22 -0500

    Adding default templates for 404, Errors, Missing Templates, etc. The templates are overrideable by like named templates in the template path.

---
+++ server/utils/error-handlers.js
@@ -0,0 +1,23 @@
+const compile = require('./compile');
+
+const fourOhFourHandler = async (req, res, template) => {
+  const compiled = await compile('404', {request:req});
+  return res.status(404).send(compiled);
+}
+
+const noTemplateHandler = async (req, res, template) => {
+  const compiled = await compile('server-missingTemplate', {request:req, template:template});
+  return res.send(compiled);
+}
+
+const noHomepageHandler = async (req, res, template) => {
+  const compiled = await compile('server-default-homepage', {request:req});
+  return res.send(compiled);
+}
+
+const noHtmlHandler = async (req, res, template) => {
+  const compiled = await compile('server-noHtml', {request:req, template:template});
+  return res.send(compiled);
+}
+
+module.exports = { fourOhFourHandler, noTemplateHandler, noHomepageHandler, noHtmlHandler};
