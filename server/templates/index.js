2a3a8541863145c8c6b7ca98aac500dd785eb595
Jason Etcovitch <jasonetco@github.com>
2018-08-15 23:02:11 -0400

    Proof of concept for GraphQL nunjucks

--- server/templates/index.js
+++ server/templates/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const nun = require('./nunjucks')
+const { graphql } = require('graphql')
+const schema = require('../schema')
 
 module.exports = (app, logger) => {
   const templates = express()
   nun.express(templates)
 
   templates.get('/', async (req, res) => {
-    res.render('index.njk')
+    const query = `{ hello }`
+    const result = await graphql(schema, query)
+    res.render('index.njk', result.data)
   })
 
   return templates
